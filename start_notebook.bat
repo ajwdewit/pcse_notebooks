8d4daf49f5d9dd87ddaff25bff7efe89f3d45e34
Allard de Wit <allard.dewit@wur.nl>
2024-06-04 13:30:25 +0200

    Modified .gitignore

--- start_notebook.bat
+++ start_notebook.bat
@@ -1,4 +1,4 @@
 set PYTHONWARNINGS=ignore
 rem set PYTHONPATH=C:\data\UserData\sources\pcse\pcse
-call C:\data\Miniconda3\Scripts\activate.bat C:\data\Miniconda3\envs\py38_notebooks
+call C:\data\Miniconda3\Scripts\activate.bat C:\data\Miniconda3\envs\py310_notebooks
 jupyter notebook
